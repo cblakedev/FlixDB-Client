990ef6ed27df5c7a699eef318bc5e4eac5631f16
Brad Titus <bltitus@me.com>
2021-07-29 13:53:28 -0400

    all watchlist functionality complete

--- src/reviews/EditWatchList.js
+++ src/reviews/EditWatchList.js
@@ -1,41 +1,16 @@
-import React, {
-    useState
-} from 'react';
+import React from 'react';
 import {
     Button,
-    Form,
-    FormGroup,
-    Label,
-    Input
+    Form
 } from 'reactstrap';
 
-const EditReviews = (props) => {
-    const [userReview, setUserReview] = useState('');
+const EditWatchList = (props) => {
     let id = props.selected.id;
     console.log(props.selected);
 
-        const handleSubmit = (e) => {
-        e.preventDefault();
-        fetch(`https://cb-movie-reviews-server.herokuapp.com/reviews/${id}`, {
-            method: 'PUT',
-            body: JSON.stringify({
-                review: {
-                    review: userReview
-                }
-            }),
-            headers: new Headers({
-                'Content-Type': "application/json",
-                'Authorization': `Bearer ${props.token}`
-            })
-        }).then((res) => res.json())
-            .then((logData) => {
-                console.log(logData);
-            })
-        }
-
         const handleDelete = (x) => {
             x.preventDefault();
-            fetch(`https://cb-movie-reviews-server.herokuapp.com/reviews/delete/${id}`, {
+            fetch(`https://cb-movie-reviews-server.herokuapp.com/watchlist/${id}`, {
                 method: 'DELETE',
                 headers: new Headers({
                     'Content-Type': 'application/json',
@@ -47,15 +22,11 @@ const EditReviews = (props) => {
 
     return (
         <div>
-            <Form className="reviewForm" onSubmit={handleSubmit}>
-                <Input id="user-review" value={userReview} label="Edit Review" type="text" onChange={(e) => setUserReview(e.target.value)} />
-                <Button className="homepageButton" id="submitReviewButton" type="submit" >Update Review</Button>
-            </Form>
             <Form className="deleteForm" onSubmit={handleDelete}>
-                <Button className="homepageButton" id="submitDeleteButton" type="submit" >Delete Review</Button>
+                <Button className="homepageButton" id="submitDeleteButton" type="submit" >Delete from List</Button>
             </Form>
         </div>
     );
 }
 
-export default EditReviews;
+export default EditWatchList;
